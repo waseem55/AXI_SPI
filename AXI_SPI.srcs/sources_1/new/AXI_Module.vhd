4162fc1c27bd90042655b11a4ca3d92a2bb716b0
waseem <waseem7446@gmail.com>
2020-05-12 15:19:49 -0500

    Write state machine done

--- AXI_SPI.srcs/sources_1/new/AXI_Module.vhd
+++ AXI_SPI.srcs/sources_1/new/AXI_Module.vhd
@@ -25,30 +25,86 @@ port(
 	
 	-- Internal Ports
 	
-	Error			: in std_logic_vector(1 downto 0)
+	Error			: in std_logic_vector(1 downto 0);
+	IntRdy			: in std_logic;
+	Wdata			: out std_logic_vector(31 downto 0);
+	Waddr			: out std_logic_vector(31 downto 0)
 	
 	);
 end AXI_Module;
 
 architecture behavior of AXI_Module is
-type write_state is (idle, awrite, dwrite, response); 
-signal wstate : write_state; 
+
+type write_state is (idle, ready, write, response); 
+signal wstate 					: write_state;		-- State machine
+signal got_addr, got_data 		: std_logic;		-- To signal receiving VALID for addr and data
+signal wdata_temp, waddr_temp 	: std_logic_vector(31 downto 0);	-- temp storage for addr and data
 
 begin
 
+	State_Machine: process(S_AXI_ACLK, S_AXI_ARESETN)
+	begin
+		if S_AXI_ARESETN = '0' then
+			wstate <= idle;
+		elsif rising_edge(S_AXI_ACLK) then
+			case wstate is
+			when idle =>
+				if S_AXI_ARESETN = '1' then
+					wstate <= ready;
+				end if;
+			when ready =>
+				if (got_addr = '1' and got_data = '1') then
+					wstate <= write;
+				end if;
+			when write =>
+				if IntRdy = '1' then
+					wstate <= response;
+				end if;
+			when response =>
+				if S_AXI_BREADY = '1' then
+					wstate <= ready;
+				end if;
+			end case;
+		end if;
+	end process;
+	
 	process(S_AXI_ACLK, S_AXI_ARESETN)
 	begin
 		if rising_edge(S_AXI_ACLK) then
-			if S_AXI_ARESETN = '0' then
-				wstate <= idle;
-			else
-				case wstate is
-				when idle =>
-					if S_AXI_
-	
-	
-	
-	
+			case wstate is
+			when idle =>
+				S_AXI_AWREADY <= '0';
+				S_AXI_WREADY <= '0';
+				S_AXI_BRESP <= "00";
+				S_AXI_BVALID <= '0';
+				got_addr <= '0';
+				got_data <= '0';
+				waddr_temp <= (others => '0');
+				wdata_temp <= (others => '0');
+				waddr <= (others => '0');
+				wdata <= (others => '0');
+			when ready =>
+				if (S_AXI_AWVALID = '1' and got_addr = '0') then
+					got_addr <= '1';
+					waddr_temp <= S_AXI_AWADDR;
+				end if;
+				if (S_AXI_WVALID = '1' and got_data = '0') then
+					got_data <= '1';
+					wdata_temp <= S_AXI_WDATA;
+				end if;
+			when write =>
+				if IntRdy = '1' then
+					waddr <= waddr_temp;
+					wdata <= wdata_temp;
+				end if;
+			when response =>
+				S_AXI_BRESP <= Error;
+				S_AXI_BVALID <= '1';
+			end case;
+		end if;
+	end process;
+			
+				
 	
 	
 	
